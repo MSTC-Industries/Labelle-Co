db5ec153d848fc8577c1a64d5f38d01ea1cb6220
MSTC-Industries <mstc.industries.official@gmail.com>
2025-05-28 22:38:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,83 @@
 var allitems = {
   //"" : {"img": "", "price": 1.00 },
   "main.html" : {
-    "Furniture" : {
+    "Tables and desks" : {
       "Dining Table" : {"img": "Images/diningtablefirst.jpg", "price": 1699 },
       //8 chairs refninished top, 2 leaves
-      "Cabinet" : {"img": "Images/salecabinet.jpg", "price": 499 },
-      "Night Stands(both!)" : {"img": "Images/nightstands.jpg", "price": 399 },
-      "Rustic Bed(Full Size!)" : {"img": "Images/rusticbed.jpg", "price": 399 },
-      "Chester Drawer" : {"img": "Images/chesterdrawer.jpg", "price": 399 },
-      "Night Stands" : {"img": "Images/nightstands2.jpg", "price": 299 },
-      "Dresser" : {"img": "Images/dresser.jpg", "price": 449 },
-      "Big Drawer" : {"img": "Images/maxstoragedrawer.jpg", "price": 399 },
-      "Small Drawer" : {"img": "Images/small cabinet.jpg", "price": 99 },
-      "3 drawer night stand" : {"img": "Images/3drawer.jpg", "price": 399 },
+      
+      
+      
       "Dining Table" : {"img": "Images/diningtable2.jpg", "price": 1699 },
       //table pads in leather case 8 chair 2 leaves
       "Wine&Cabinet" : {"img": "Images/wineandcabindet.jpg", "price": 449 },
       "Side table" : {"img": "Images/sidetable.jpg", "price": 199 },
-      "Iron king bed" : {"img": "Images/ironking.jpg", "price": 795 },
+      
       //welded no squeec no screws
-      "Tall Cabinet" : {"img": "Images/tallcabinet.jpg", "price": 299 },
+      
       "French Blue Desk" : {"img": "Images/antiquebluetable.jpg", "price": 389 },
       "Queen Bed" : {"img": "Images/queenbed.jpg", "price": 599 },
+      
+      
+      
+      
+      "Writing Desk" : {"img": "Images/writingdesk.jpg", "price": 299 },
+      //bamboo chair
+      
+      
+      "Coffee Table" : {"img": "Images/coffeetable.jpg", "price": 399 },
+      
+      "White Coffee Table" : {"img": "Images/coffeetblwhite.jpg", "price": 299 },
+      
+      "Wooden Dinner Table" : {"img": "Images/woodentable.jpg", "price": 1299 },
+      //leaves
+      
+      "Small Table" : {"img": "Images/smalltable.jpg", "price": 1895 },
+      
+      
+      
+    },
+    "Cabinets" : {
+        "Cabinet" : {"img": "Images/salecabinet.jpg", "price": 499 },
+      "Skinny Cabinets" : {"img": "Images/smallcabs.jpg", "price": 299 },
+      "Night Stands(both!)" : {"img": "Images/nightstands.jpg", "price": 399 },
       "Two Drawer Cabinet" : {"img": "Images/2layercab.jpg", "price": 299 },
-      "Wardrobe" : {"img": "Images/Wardrobe.jpg", "price": 399 },
-      "King Bed" : {"img": "Images/Kingbed.jpg", "price": 599 },
       "Regency Style Dresser" : {"img": "Images/regencystyledresser.jpg", "price": 549 },
       "Five layer Cabinet" : {"img": "Images/5layercab.jpg", "price": 299 },
       "Big Blue Cabinet" : {"img": "Images/bigbluecab.jpg", "price": 399 },
       "Blue Bookshelf" : {"img": "Images/smallbluecab.jpg", "price": 499 },
       "Beutifal Cabinet" : {"img": "Images/bigbeutifalcabinet.jpg", "price": 529 },
       "Green Chest" : {"img": "Images/bigbeutifalchest.jpg", "price": 499 },
-      "Red China Hutch" : {"img": "Images/redcabinet.jpg", "price": 249 },
-      "Writing Desk" : {"img": "Images/writingdesk.jpg", "price": 299 },
-      //bamboo chair
       "Hand made bookshelf" : {"img": "Images/handmadebookshelf.jpg", "price": 399 },
-      "Lamp" : {"img": "Images/lamp.jpg", "price": 75.95 },
-      "Coffee Table" : {"img": "Images/coffeetable.jpg", "price": 399 },
-      "Skinny Cabinets" : {"img": "Images/smallcabs.jpg", "price": 299 },
-      "White Coffee Table" : {"img": "Images/coffeetblwhite.jpg", "price": 299 },
-      "Queen Bed" : {"img": "Images/queenbed2.jpg", "price": 695 },
-      "Wooden Dinner Table" : {"img": "Images/woodentable.jpg", "price": 1299 },
-      //leaves
+      "Red China Hutch" : {"img": "Images/redcabinet.jpg", "price": 249 },
+      "Tall Blue Cabinet" : {"img": "Images/TallblueCabinet.jpg", "price": 549 },
+      //Original Bamboo
+      "Ballerina cabinet" : {"img": "Images/ballerinacab.jpg", "price": 349 },
+      "Wardrobe" : {"img": "Images/Wardrobe.jpg", "price": 399 },
+      "Chester Drawer" : {"img": "Images/chesterdrawer.jpg", "price": 399 },
+      "Night Stands" : {"img": "Images/nightstands2.jpg", "price": 299 },
+      "Dresser" : {"img": "Images/dresser.jpg", "price": 449 },
+      "Big Drawer" : {"img": "Images/maxstoragedrawer.jpg", "price": 399 },
+      "Small Drawer" : {"img": "Images/small cabinet.jpg", "price": 99 },
+      "3 drawer night stand" : {"img": "Images/3drawer.jpg", "price": 399 },
+      "Wine&Cabinet" : {"img": "Images/wineandcabindet.jpg", "price": 449 },
       "Grated Night Stand(2x)" : {"img": "Images/gratenightstand.jpg", "price": 399 },
       "Dresser" : {"img": "Images/large dresser.jpg", "price": 489 },
       "White Chester Drawers" : {"img": "Images/chesterdrawer2.jpg", "price": 399 },
       "Lingerie chest" : {"img": "Images/Tallcabinet2.jpg", "price": 299 },
       "Large Belgian Cabinet" : {"img": "Images/bigwoodenboy.jpg", "price": 1395 },
-      "Small Table" : {"img": "Images/smalltable.jpg", "price": 1895 },
-      "Tall Blue Cabinet" : {"img": "Images/TallblueCabinet.jpg", "price": 549 },
-      //Original Bamboo
-      "Ballerina cabinet" : {"img": "Images/ballerinacab.jpg", "price": 349 },
-      "Squash Memory Books" : {"img": "Images/squashmemorybooks.jpg", "price": 22 },
+      "Tall Cabinet" : {"img": "Images/tallcabinet.jpg", "price": 299 },
+      "Wine&Cabinet" : {"img": "Images/wineandcabindet.jpg", "price": 449 },
+    },
+    "Beds" : {
+        "Rustic Bed(Full Size!)" : {"img": "Images/rusticbed.jpg", "price": 399 },
+        "King Bed" : {"img": "Images/Kingbed.jpg", "price": 599 },
+        "Queen Bed" : {"img": "Images/queenbed2.jpg", "price": 695 },
+        "Iron king bed" : {"img": "Images/ironking.jpg", "price": 795 },
+    },
+    "Other Items" : {
+        "Squash Memory Books" : {"img": "Images/squashmemorybooks.jpg", "price": 22 },
       "Geometry Towels" : {"img": "Images/geotowels.jpg", "price": 22 },
-      
+      "Lamp" : {"img": "Images/lamp.jpg", "price": 75.95 },
     },
   },
 }
